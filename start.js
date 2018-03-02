8528feaa2d5951232a16d91fcf278454005b4340
just call me azure <10553730+Azure-Agst@users.noreply.github.com>
2018-03-02 08:15:10 -0500

    this is all that remains.
    This was on my clipboard. This is all that survived, alongside one image I put on twitter. ¯\_(ツ)_/¯

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 	var images = files.filter(word => word.endsWith(".jpeg")||word.endsWith(".jpg")||word.endsWith(".png")); var il2 = "";
 	for (i=0; i<images.length; i++) {
-		il2 += "<img src='./images/"+images[i]+"'>;
+		il2 += "<img src='./images/"+images[i]+"'>";
 	}
     return res.render('page2', {images: il2});
