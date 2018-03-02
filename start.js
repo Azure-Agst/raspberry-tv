b8725963cec4e3fe8d95be3d1a51f4499c3c5027
Azure-Agst <Azure-Agst@users.noreply.github.com>
2018-03-02 18:17:55 +0000

    upload/delete handling added

--- start.js
+++ start.js
@@ -22,23 +22,43 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 // Consider using a switch statement here
 
 app.all('/', function (req, res) {
-	var form = new formidable.IncomingForm();
-	form.parse(req, function (err, fields, files) {
-		if (files.filetoupload == undefined) {postprocessing(); return;};
-		var oldpath = files.filetoupload.path;
-		var newpath = __dirname + "/images/" + files.filetoupload.name;
-		fs.rename(oldpath, newpath, function (err) {
-			if (err) throw err;
-			postprocessing();
+	if (req.method == 'POST') {
+		var form = new formidable.IncomingForm();
+		form.parse(req, function (err, fields, files) {
+			//console.log(fields);console.log(files);
+			
+			//determine what form
+			if (files.filetoupload !== undefined) { // it's an upload
+				var oldpath = files.filetoupload.path; var filename;
+				if (fields.rename == undefined) {filename == files.filetoupload.name;} else {filename = fields.rename};
+				if (filename.endsWith(".jpg") == false || filename.endsWith(".jpeg") == false || filename.endsWith(".png") == false) filename += "."+files.filetoupload.name.split('.').pop();
+				var newpath = __dirname + "/images/" + filename;
+				fs.rename(oldpath, newpath, function (err) {
+					if (err) throw err;
+					postprocessing();
+					return;
+				});
+			} else if (fields.filedelete !== null) {
+				fs.unlink('./images/'+fields.filedelete, function(err){
+					if (err) throw err;
+					postprocessing();
+					return;
+				});
+			} else {
+				res.write("what.");
+				return res.end();
+			}
 		});
-	});
+	} else {
+		postprocessing();
+	}
 
 	function postprocessing(){
 		fs.readdir('./images/', function(err, files){
 			var images = files.filter(word => word.endsWith(".jpeg")||word.endsWith(".jpg")||word.endsWith(".png"))
 			var imagelist = "";
 			for (i=0; i<images.length; i++) {
-				imagelist += "<div class='row'><div class='column1'><p>"+images[i]+"</p></div><div class='column2'><img src ='./images/"+images[i]+"' style='float: right;' height='100px'></div><div class='column3'><button class='btn btn-danger' style='float: right;'>X</button></div></div>";
+				imagelist += "<div class='row'><div class='column1'><p>"+images[i]+"</p></div><div class='column2'><img src ='./images/"+images[i]+"' style='float: right;' height='100px'></div><div class='column3'><form method='post' enctype='multipart/form-data'><button class='btn btn-danger' name='filedelete' value='"+images[i]+"' style='float: right;'>X</button></form></div></div>";
 			}
 			return res.render('home', {images: imagelist});
 		})
